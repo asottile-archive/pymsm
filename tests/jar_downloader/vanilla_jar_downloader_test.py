78298da7cfc10367ae3682df42919cdb6b82cf99
Anthony Sottile <asottile@umich.edu>
2013-08-10 19:39:31 -0400

    Added external smoke test for version retrieval.

--- tests/jar_downloader/vanilla_jar_downloader_test.py
+++ tests/jar_downloader/vanilla_jar_downloader_test.py
@@ -27,5 +27,34 @@ class TestGetVersionsJson(T.TestCase):
             )
             T.assert_equal(retval, loads_mock.return_value)
 
+    @T.suite('integration')
+    @T.suite('external')
+    def test_structure_of_external_json(self):
+        json_object = get_versions_json()
+
+        T.assert_equal(
+            json_object,
+            {
+                'versions': mock.ANY, # Tested more explicitly below
+                'latest': {
+                    'release': mock.ANY,
+                    'snapshot': mock.ANY,
+                }
+            }
+        )
+
+        # Versions should be a list of dict objects
+        T.assert_isinstance(json_object['versions'], list)
+        for version_dict in json_object['versions']:
+            T.assert_equal(
+                version_dict,
+                {
+                    'releaseTime': mock.ANY,
+                    'type': mock.ANY,
+                    'id': mock.ANY,
+                    'time': mock.ANY,
+                }
+            )
+
 class TestVanillaJarDownloader(T.TestCase):
     pass
