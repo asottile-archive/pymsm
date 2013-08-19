0360317aab2205d09ab005fc0ccbb2b7e088a846
Anthony Sottile <asottile@umich.edu>
2013-08-18 22:58:35 -0400

    Added and started testing jar downloader discovery.

---
+++ jar_downloader/discovery.py
@@ -0,0 +1,47 @@
+
+import fnmatch
+import inspect
+import os
+import os.path
+import sys
+
+from jar_downloader.jar_downloader_base import JarDownloaderBase
+
+JAR_DOWNLOADER_DIRECTORY = os.path.dirname(__file__)
+
+def get_module_name(root, filename):
+    if not filename.endswith('.py'):
+        raise ValueError('filename must end with .py')
+    filename = filename[:-3]
+    # XXX: should really use pathsep here
+    return os.path.join(root, filename).replace('/', '.')
+
+def is_jar_downloader(cls):
+    return (
+        cls is not JarDownloaderBase and
+        issubclass(cls, JarDownloaderBase) and
+        getattr(cls, '__jar_downloader__', True)
+    )
+
+def get_jar_downloaders():
+    """Returns a list of classes that are JarDownloaders."""
+
+    jar_downloaders = set()
+
+    # Look for all python files in the jar downloader directory
+    for root, _, filenames in os.walk(JAR_DOWNLOADER_DIRECTORY):
+        for filename in filenames:
+            if fnmatch.fnmatch(filename, '*.py'):
+                module_name = get_module_name(root, filename)
+                # Import the module
+                __import__(module_name)
+                module = sys.modules[module_name]
+
+                # Check all the classes in that module
+                for name, _ in inspect.getmembers(module, inspect.isclass):
+                    imported_class = getattr(module, name)
+
+                    if is_jar_downloader(imported_class):
+                        jar_downloaders.add(imported_class)
+
+    return jar_downloaders
---
+++ tests/jar_downloader/discovery_test.py
@@ -0,0 +1,40 @@
+
+import testify as T
+
+from jar_downloader.discovery import get_module_name
+from jar_downloader.discovery import is_jar_downloader
+from jar_downloader.jar_downloader_base import JarDownloaderBase
+
+class TestGetModuleName(T.TestCase):
+    """Tests the get_module_name function."""
+
+    def test_get_module_name(self):
+        module_name = get_module_name('foo', 'bar.py')
+        T.assert_equal(module_name, 'foo.bar')
+
+    def test_raises_on_non_python_file(self):
+        with T.assert_raises(ValueError):
+            get_module_name('foo', 'bar')
+
+    def test_more_complicated_directory(self):
+        module_name = get_module_name('foo/bar', 'baz.py')
+        T.assert_equal(module_name, 'foo.bar.baz')
+
+class TestIsJarDownloader(T.TestCase):
+    """Tests the is_jar_downloader method."""
+
+    def test_jar_downloader_base_is_not_one(self):
+        T.assert_equal(is_jar_downloader(JarDownloaderBase), False)
+
+    def test_excluded_jar_downloader_is_not_one(self):
+        class ImNotAJarDownloader(JarDownloaderBase):
+            __jar_downloader__ = False
+
+        T.assert_equal(is_jar_downloader(ImNotAJarDownloader), False)
+
+    def test_some_randomass_class_is_not_a_jar_downloader(self):
+        T.assert_equal(is_jar_downloader(object), False)
+
+    def test_some_object_that_should_be_a_jar_downloader(self):
+        class ImAJarDownloader(JarDownloaderBase): pass
+        T.assert_equal(is_jar_downloader(ImAJarDownloader), True)
