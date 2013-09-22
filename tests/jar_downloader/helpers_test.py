52c9717393516fec9a5075e56c877c48958b081e
Anthony Sottile <asottile@umich.edu>
2013-09-22 14:29:02 -0700

    Added integration test (for yesterdays regression).

--- tests/jar_downloader/helpers_test.py
+++ tests/jar_downloader/helpers_test.py
@@ -5,12 +5,15 @@ import contextlib
 import mock
 import os
 import os.path
+import shutil
 import simplejson
+import tempfile
 import testify as T
 
 import config.application
 from jar_downloader.helpers import CONFIG_FILE
 from jar_downloader.helpers import create_jar_directory
+from jar_downloader.jar_downloader_base import JarDownloaderBase
 from testing.utilities.fake_file import FakeFile
 
 class TestCreateJarDirectory(T.TestCase):
@@ -60,3 +63,32 @@ class TestCreateJarDirectory(T.TestCase):
             self.open_mock.return_value._written_contents,
             simplejson.dumps(jar_config)
         )
+
+@T.suite('integration')
+class TestCreateJarDirectoryIntegration(T.TestCase):
+
+    @T.setup_teardown
+    def mock_jar_directory(self):
+        self.tempdir = tempfile.mkdtemp()
+        try:
+            with mock.patch.object(
+                config.application,
+                'JARS_PATH',
+                self.tempdir,
+            ):
+                yield
+        finally:
+            shutil.rmtree(self.tempdir)
+
+    def test_create_jar_directory_config_read(self):
+        configuration = {'herp': 'derp'}
+        create_jar_directory('herp', 'derp', configuration)
+
+        jar_path = os.path.join(self.tempdir, 'herp', 'derp')
+        T.assert_equal(
+            True,
+            os.path.exists(jar_path),
+        )
+
+        jar_instance = JarDownloaderBase(jar_path)
+        T.assert_equal(jar_instance.config, configuration)
