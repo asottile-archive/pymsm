17a9567469fa07841dc3d7f7db291fdcbb5695da
Anthony Sottile <asottile@umich.edu>
2013-10-07 21:03:25 -0700

    Server now uses util.properties

--- server/user_server.py
+++ server/user_server.py
@@ -1,39 +1,9 @@
 
 import os.path
-import simplejson
 
-from jar_downloader.discovery import get_jar_downloaders
+from util.properties import Properties
 
 SERVER_PROPERTIES_FILENAME = 'server.properties'
-
-CONFIG_FILENAME = 'config.json'
-
-CONFIG_SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'jar': {
-            'type': 'object',
-            'properties': {
-                'jar_type': {
-                    'type': 'string',
-                    # jar_type is a choice of the types of jars defined
-                    # This can be statically defined here because the jar types
-                    # don't change at runtime, however user_jar_name cannot
-                    # be checked in this way and must need a runtime check
-                    'enum': [
-                       jar_downloader_cls.__name__
-                       for jar_downloader_cls in get_jar_downloaders()
-                    ],
-                },
-                'user_jar_name': {
-                    'type': 'string',
-                },
-            },
-            'required': ['jar_type', 'user_jar_name'],
-        },
-    },
-    'required': ['jar'],
-}
 
 class UserServer(object):
     """Class repesenting a user's server.  A server has settings to configure
@@ -45,10 +15,10 @@ class UserServer(object):
         self.server_dir = server_dir
 
     @property
-    def config_path(self):
-        return os.path.join(self.server_dir, CONFIG_FILENAME)
+    def server_properties_path(self):
+        return os.path.join(self.server_dir, SERVER_PROPERTIES_FILENAME)
 
     @property
-    def config(self):
-        with open(self.config_path, 'r') as config_file:
-            return simplejson.load(config_file)
+    def server_properties(self):
+        with open(self.server_properties_path, 'r') as server_properties_file:
+            return Properties.load(server_properties_file)
