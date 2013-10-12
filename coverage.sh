c44430b488aa573fafccbd9320888a16843e276a
Anthony Sottile <asottile@umich.edu>
2013-10-11 20:01:05 -0700

    Ignore built mako modules (coverage seems to choke on them since they are temporary)

--- coverage.sh
+++ coverage.sh
@@ -6,5 +6,5 @@ coverage erase
 coverage run fix_coverage.py
 coverage run `which testify` tests $@
 coverage combine
-coverage report -m --omit=/usr/*,*/__init__.py,tests/*,pre-commit.py
+coverage report -m --omit=/usr/*,*/__init__.py,tests/*,pre-commit.py,*_mako
 exit $?
