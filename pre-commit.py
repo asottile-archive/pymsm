25c5795e876c658b8e6e26d27597244da667e77c
Anthony Sottile <asottile@umich.edu>
2013-11-06 10:08:30 -0800

    backport check_output

--- pre-commit.py
+++ pre-commit.py
@@ -8,6 +8,34 @@ import shutil
 import subprocess
 import sys
 
+def __backport_check_output():
+    def check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+
+        Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+        >>> check_output(['/usr/bin/python', '--version'])
+        Python 2.6.2
+        """
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            error = subprocess.CalledProcessError(retcode, cmd)
+            error.output = output
+            raise error
+        return output
+
+    if not hasattr(subprocess, 'check_output'):
+        setattr(subprocess, 'check_output', check_output)
+
+__backport_check_output()
+del __backport_check_output
+
+
 FILE_LIST = 'git ls-files | egrep "\.%s$"'
 
 ALL_FILES = FILE_LIST % '(php|sql|py|js|htm|c|cpp|h|sh|css)'
